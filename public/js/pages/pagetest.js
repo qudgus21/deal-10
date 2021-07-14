c8c18be7fae34484e2aa95845417f94e70e84df8
황병현 <>
2021-07-14 10:45:07 +0900

    수정: reset.scss

--- public/js/pages/pagetest.js
+++ public/js/pages/pagetest.js
@@ -1,29 +1,24 @@
-cimport comp from '../components/compotest.js'
-import api from '../../utils/api.js'
-import { LoaderTargetPlugin } from 'webpack';
+import comp from '../components/compotest.js';
+import api from '../../utils/api.js';
 
-export default function pagetest(props) { 
+export default function pagetest(props) {
+  this.state = {
+    productList: [],
+  };
 
-    this.state = {
-        productList : [],
-    }
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
 
+  // this.complete = (text) => {
+  //   this.setState({
+  //     messsage: text,
+  //   });
+  // };
 
-    this.setState = (nextState) => { 
-        this.state = nextState;
-        this.render();
-    }
-
-
-    this.complete = (text) => { 
-        this.setState({
-            messsage: text
-        })
-    }
-
-
-    this.render = () => {
-        let templateLiteral = `
+  this.render = () => {
+    let templateLiteral = `
             <div class="pagetest">
                 <div class="msg-container">
                     
@@ -34,46 +29,34 @@ export default function pagetest(props) {
             </div>
         `;
 
-        props.parent.innerHTML = templateLiteral;
-        
+    props.parent.innerHTML = templateLiteral;
 
-        Array.from(props.parent.querySelectorAll('.btn')).pop().addEventListener('click', () => {
-            const params = {
-                userIdx : '1'
-            }
+    Array.from(props.parent.querySelectorAll('.btn'))
+      .pop()
+      .addEventListener('click', () => {
+        const params = {
+          userIdx: '1',
+        };
 
-            api.sendPost('/user/register', params).then(result => { 
-                if (result.status = 'ok') {
-                    this.setState({
-                        
-                    })
-                }
+        api.sendPost('/user/register', params).then((result) => {
+          if ((result.status = 'ok')) {
+            this.setState({});
+          }
 
+          window.history.pushState('v1', '', '/page1');
 
+          if ('/login') {
+            new login({ parent: document.querySelector('') });
+          }
+        });
+      });
 
+    // this.state.productList.map(function(msg) {
+    //     new comp({ parent: document.querySelector('.msg-container'), message: msg });
+    // });
+    // new comp({ parent: document.querySelector('.second'), message: '두번째', testfunction : this.complete});
+    // new comp({ parent: document.querySelector('.third'), message: '세번째' });
+  };
 
-
-
-
-
-
-                window.history.pushState('v1', '', '/page1');
-                
-                
-                if ('/login') {
-                    new login({parent: document.querySelector("")})
-                }
-            })
-        })
-
-        // this.state.productList.map(function(msg) {
-        //     new comp({ parent: document.querySelector('.msg-container'), message: msg });
-        // });
-        // new comp({ parent: document.querySelector('.second'), message: '두번째', testfunction : this.complete});
-        // new comp({ parent: document.querySelector('.third'), message: '세번째' });
-        
-        
-    }  
-    
-    this.render();
+  this.render();
 }
