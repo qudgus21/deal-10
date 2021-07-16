0adb1813a1eb8ec564b005f7a29e3eb4282ea09f
황병현 <>
2021-07-16 21:56:23 +0900

    productdetail slide

--- client/pages/SaleProductDetail/ProductDetail.js
+++ client/pages/SaleProductDetail/ProductDetail.js
@@ -3,19 +3,57 @@ import { slideIn, slideOut } from '../../utils/slide';
 import WithoutAction from '../../components/Header/WithoutAction';
 import Button from '../../components/Button/Button';
 import TextInput from '../../components/TextInput/TextInput';
+
+import withAction from '../../components/Header/withAction';
+
 import api from '../../utils/api';
 
 export default function ProductDetail(props) {
   this.state = {
-    value: '',
+    product: null,
   };
 
   this.setState = (nextState) => {
+    setTimeout(() => {
+      document.querySelector('.app').lastElementChild.remove();
+    }, 100);
     this.state = nextState;
     this.render();
+    setTimeout(() => {
+      document.querySelector('.app').lastElementChild.classList.add('slide-in');
+    }, 50);
   };
 
-  console.log(window.location.pathname);
+  setTimeout(() => {
+    const product = window.location.pathname.split('/').pop();
+
+    this.setState({
+      product: {
+        isSaler: -1,
+        isLike: 'N',
+        salerId: 'qudgus21',
+        location: '역삼동',
+        chattCnt: '10',
+        likeCnt: '5',
+        viewCnt: '12',
+        title: '테스트 제목',
+        description: '가짜 설명',
+        price: '기갹',
+        imgUrls: ['berry', 'berry', 'berry', 'berry', 'berry'],
+        status: 'S',
+        updateDate: '2시간 전',
+      },
+    });
+    // api
+    //   .sendPost('/product/categoryProducts', { categoryIdx: category })
+    //   .then((result) => {
+    //     this.setState({
+    //       ...this.state,
+    //       products: result.data.products,
+    //       category: result.data.category,
+    //     });
+    //   });
+  }, 0);
 
   this.render = () => {
     let templateLiteral = `
@@ -28,6 +66,14 @@ export default function ProductDetail(props) {
         `;
 
     props.parent.insertAdjacentHTML('beforeend', templateLiteral);
+
+    new withAction({
+      parent: document.querySelector('.productdetail .header-box'),
+      content: this.state.product ? this.state.product.title : '',
+      eventHandler: (e) => {
+        window.history.back();
+      },
+    });
   };
 
   this.render();
