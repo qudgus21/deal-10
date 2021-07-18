c81dd81a339cdea37868efa3ba8e1637c040c905
황병현 <>
2021-07-18 19:20:38 +0900

    수정: 로그아웃 메세지

--- client/pages/MyAccount/MyAccount.js
+++ client/pages/MyAccount/MyAccount.js
@@ -42,6 +42,7 @@ export default function MyAccount(props) {
           if (result.status === 'ok') {
             setCookie('user', 'none', 0);
             setCookie('userIdx', 'none', 0);
+            alert('로그아웃 성고');
             window.location.href = '/';
             // slideOut('/', false);
           }
