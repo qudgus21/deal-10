736add3225cff53ad5ee5cd62fbe614acc3965e4
negu63 <dnjsdn2019@gmail.com>
2021-07-19 14:23:35 +0900

    수정: 채팅 목록 아이템 파라미터

--- client/components/Etc/ChatListItem.js
+++ client/components/Etc/ChatListItem.js
@@ -1,4 +1,5 @@
 import { selectLatestElement } from '../../utils/helper';
+import { slideIn } from '../../utils/slide';
 
 export default function ChatListItem(props) {
   this.state = {
@@ -11,27 +12,45 @@ export default function ChatListItem(props) {
   };
 
   this.render = () => {
-    const { chat } = props;
+    const {
+      productId,
+      status,
+      opponent,
+      currentMessage,
+      unreadCnt,
+      imgUrls,
+      updatetime,
+    } = this.state.data;
 
     let templateLiteral = `
         <div class='chat-list-item'>
             <div class='chat-div'>
                 <div class='chat-row'>
-                    <div class='chat-id'>${chat.userId}</div>
-                    <div class='chat-agoTime'>${chat.agoTime}</div>
+                    <div class='chat-id'>${opponent.userName}</div>
+                    <div class='chat-agoTime'>${updatetime}</div>
                 </div>
                 <div class='chat-row'>
-                    <div class='chat-preview'>${chat.preview}</div>
-                    <div class='chat-badge'>${chat.badge}</div>
+                    <div class='chat-preview'>${currentMessage}</div>
+                    <div class='chat-badge'>${unreadCnt}</div>
                 </div>
             </div>
             <div class='img-box'>
-                <img src='${chat.imgUrl}' class='border-small'>
+                <img src='${imgUrls[0]}' class='border-small'>
             </div>
         </div>
     `;
 
     props.parent.insertAdjacentHTML('beforeend', templateLiteral);
+
+    const $chatListItem = selectLatestElement(
+      document.querySelector('.chat-list-item'),
+      '.chat-div'
+    );
+    $chatListItem.addEventListener('click', () => {
+      // product/${productId}/chatting/${chatting-contentsId}
+      // product id랑 chattingcontentsid도 받아야할듯
+      slideIn(`/product/${productId}/chatting/${chattingContentsId}`, false);
+    });
   };
 
   this.render();
