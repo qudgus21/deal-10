898a7a66c60f28ff3d240558caadc0eedda7e972
황병현 <>
2021-07-22 00:45:47 +0900

    수정: 메세지 최신

--- server/db.js
+++ server/db.js
@@ -7,7 +7,6 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  timezone: process.env.DB_TIMEZONE,
   database: process.env.DB_DATABASE,
   connectionLimit: 500,
 });
